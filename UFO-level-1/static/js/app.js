931e2f51fc9213745bf5c78ce107cfc1202fb6cc
Jordan Thompson <59570390+thom0497@users.noreply.github.com>
2020-06-22 13:26:44 -0500

    final assignment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-group");
+var tbody = d3.select("tbody");
+
+
+
+// Give the button and form a function to run on an event
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select(".form-control");
+
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    tbody.html("")
+
+    filteredData.forEach(function(sightingsList) { 
+
+        var row = tbody.append("tr");
+        
+        Object.entries(sightingsList).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value)
+
+    })}
+)};
